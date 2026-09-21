5cafa1e06a4ba7c4bb2b6c733ba87835ec61dae5
asw4063 <131714632+asw4063@users.noreply.github.com>
2026-09-21 10:02:33 -0600

    Grades First Draft

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+        string input;
+        Console.Write("What is your score? (Please enter your answer as a percentage) ");
+        input = Console.ReadLine();
+        int score = int.Parse(input);
+
+
+        if (score > 130)
+        {
+            Console.WriteLine($" {score}?! I think you're lying.");
+        
+        }
+
+        else if (score > 100)
+        {
+            Console.WriteLine($"Wow! That's a lot of extra credit! Definitly an A++");
+        }
+
+        else if (score > 90)
+        {
+            Console.WriteLine($"Congratulaions!! You got an A");
+
+        }
+
+        else if (score > 80 )
+        {
+            Console.WriteLine($"Not bad! B.");
+
+        }
+
+        else if (score > 70)
+        {
+            Console.WriteLine($"That earns you a C. Congratulations! You passed!");
+
+        }
+
+        else if (score > 60)
+        {
+            Console.WriteLine($"I'm so sorry. a D doesn't pass");
+        }
+
+        else
+        {
+            Console.WriteLine($"You flunked. :(( You'll have to retake the class");
+        }
+
+
+
+
+        
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,12 +1,31 @@
 using System;
+using System.IO.Compression;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Bonbon");
-        Console.WriteLine("Bonbon");
-        Console.WriteLine("Bonbon");
+
+        int x = 20;
+        int y = 20;
+        int z = 30;
+
+        if (x==10 || y ==20 && z ==30 )
+        {
+            Console.WriteLine("X is 10");
+            Console.WriteLine("Y is fun");
+        }
+
+        else if (x==20)
+        {
+            Console.Write("We are in the else if");
+        }
+        else
+        {
+            Console.Write("Z if not much fun");
+        }
+
     }
 
 
