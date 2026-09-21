57073c2d57d9afe1b59f0d67efaedfad269ea62b
asw4063 <131714632+asw4063@users.noreply.github.com>
2026-09-21 10:38:26 -0600

    Grade program Improvement

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -6,48 +6,82 @@ class Program
     {
         
         string input;
-        Console.Write("What is your score? (Please enter your answer as a percentage) ");
+        Console.Write("What is your score? (Please enter the percentage as an integer) ");
         input = Console.ReadLine();
         int score = int.Parse(input);
 
 
         if (score > 130)
         {
+            string letterGrade = "A+++";
             Console.WriteLine($" {score}?! I think you're lying.");
+
         
         }
 
         else if (score > 100)
         {
-            Console.WriteLine($"Wow! That's a lot of extra credit! Definitly an A++");
+            string letterGrade = "A++";
+            Console.WriteLine($"Wow! That's a lot of extra credit! Definitly an {letterGrade}");
         }
 
-        else if (score > 90)
+        else if (score >= 90)
         {
-            Console.WriteLine($"Congratulaions!! You got an A");
+      
+            if (score >= 97)
+            {
+                string letterGrade = "A+";
+                Console.WriteLine($"Congratulaions!! You got an {letterGrade}");
+            }
+
+            else if (score> 92)
+            {
+                string letterGrade = "A";
+                Console.WriteLine($"Congratulaions!! You got an {letterGrade}");
+            }
+
+            else if (score < 93)
+            {
+                string letterGrade = "A-";
+                Console.WriteLine($"Congratulaions!! You got an {letterGrade}");
+            }
+
+            else
+            {
+                string letterGrade = "A-";
+                Console.WriteLine($"Congratulaions!! You got an {letterGrade}");
+            }
+
+            
 
         }
 
-        else if (score > 80 )
+        else if (score >= 80 )
         {
-            Console.WriteLine($"Not bad! B.");
+            string letterGrade = "B";
+            Console.WriteLine($"Not bad! {letterGrade}.");
 
         }
 
-        else if (score > 70)
+        else if (score >= 70)
         {
             Console.WriteLine($"That earns you a C. Congratulations! You passed!");
 
         }
 
-        else if (score > 60)
+        else if (score >= 60)
         {
-            Console.WriteLine($"I'm so sorry. a D doesn't pass");
+            Console.WriteLine($"I'm so sorry. A D doesn't pass.");
+        }
+
+        else if (score <60 && score > 0)
+        {
+            Console.WriteLine($"You flunked. :(( You'll have to retake the class");
         }
 
         else
         {
-            Console.WriteLine($"You flunked. :(( You'll have to retake the class");
+            Console.WriteLine($"I'm not sure what {score} means, but I think you're cooked.");
         }
 
 
